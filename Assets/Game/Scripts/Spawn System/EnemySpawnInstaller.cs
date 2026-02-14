4a6e89bbf862e82155f186ba3d361b3d00857a82
AleProfJunior <alexandre.junior@edu.sc.senai.br>
2026-02-14 19:08:13 -0300

    Fix: enemy catalog inspector error reference

--- Assets/Game/Scripts/Spawn System/EnemySpawnInstaller.cs
+++ Assets/Game/Scripts/Spawn System/EnemySpawnInstaller.cs
@@ -13,23 +13,14 @@ namespace Game.SpawnSystem
         {
             Container.BindInstance(enemyCatalogRegistry.GenerateDictionary());
             Container.BindFactory<EnemyType, EnemyController, EnemyFactory>().FromMethod(CreateEnemy);
-            //CreateEnemyFunc += CreateEnemy;
-        }
-
-        private void OnEnable()
-        {
-           
-        }
-        private void OnDisable()
-        {
-            //CreateEnemyFunc -= CreateEnemy;
         }
 
         private EnemyController CreateEnemy(DiContainer container, EnemyType type)
         {
             var dictionary = Container.Resolve<Dictionary<EnemyType, EnemyController>>();
-            if (dictionary.TryGetValue(type, out var enemy)) Debug.Log("Enemy Found");
-            return Container.InstantiatePrefabForComponent<EnemyController>(dictionary[type]);
+            if (!dictionary.TryGetValue(type, out var enemy)) 
+                Debug.LogError($"Enemy not Found by key {type}");
+            return container.InstantiatePrefabForComponent<EnemyController>(enemy);
         }
     }
 }
