d0570a572dfd8fe0b4f8eb8eb5cf2a4b6e25c9b0
AleProfJunior <alexandre.junior@edu.sc.senai.br>
2026-02-15 04:52:51 -0300

    Enemy factory installer as generic

--- Assets/Game/Scripts/Spawn System/EnemySpawnInstaller.cs
+++ Assets/Game/Scripts/Spawn System/EnemySpawnInstaller.cs
@@ -1,45 +1,17 @@
-﻿using System.Collections.Generic;
-using Game.Characters;
-using UnityEngine;
-using Zenject;
+﻿using Game.Characters;
 
 namespace Game.SpawnSystem
 {
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Zenject;
     
-    // public class SpawnSystemInstaller<TEnum, TController> : MonoInstaller where TEnum : System.Enum where TController : MonoBehaviour
-    // {
-    //     [SerializeField] private EnemyCatalogRegistry enemyCatalogRegistry;
-    //     public override void InstallBindings()
-    //     {
-    //         Container.BindInstance(enemyCatalogRegistry.GenerateDictionary());
-    //         Container.BindFactory<EnemyType, EnemyController, EnemyFactory>().FromMethod(CreateEnemy);
-    //     }
-    //
-    //     private EnemyController CreateEnemy(DiContainer container, EnemyType type)
-    //     {
-    //         var dictionary = Container.Resolve<Dictionary<EnemyType, EnemyController>>();
-    //         if (!dictionary.TryGetValue(type, out var enemy)) 
-    //             Debug.LogError($"Enemy not Found by key {type}");
-    //         return container.InstantiatePrefabForComponent<EnemyController>(enemy);
-    //     }
-    // }
-    //
-    
-    public class EnemySpawnInstaller : MonoInstaller
+    public class EnemyFactoryInstaller 
+        : FactoryInstaller<EnemyType, EnemyController>
     {
-        [SerializeField] private EnemyCatalogRegistry enemyCatalogRegistry;
-        public override void InstallBindings()
-        {
-            Container.BindInstance(enemyCatalogRegistry.GenerateDictionary());
-            Container.BindFactory<EnemyType, EnemyController, PlaceholderFactory<EnemyType,EnemyController>>().FromMethod(CreateEnemy);
-        }
+        [SerializeField] EnemyCatalogRegistry _registry;
 
-        private EnemyController CreateEnemy(DiContainer container, EnemyType type)
-        {
-            var dictionary = Container.Resolve<Dictionary<EnemyType, EnemyController>>();
-            if (!dictionary.TryGetValue(type, out var enemy)) 
-                Debug.LogError($"Enemy not Found by key {type}");
-            return container.InstantiatePrefabForComponent<EnemyController>(enemy);
-        }
+        protected override Dictionary<EnemyType, EnemyController> BuildDictionary()
+            => _registry.GenerateDictionary();
     }
 }
---
+++ Assets/Game/Scripts/Spawn System/FactoryInstaller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Game.Characters;
+using UnityEngine;
+using Zenject;
+
+namespace Game.SpawnSystem
+{
+    public abstract class FactoryInstaller<TKey, TContract> : MonoInstaller
+        where TContract : Component
+    {
+        protected abstract Dictionary<TKey, TContract> BuildDictionary();
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(BuildDictionary()).AsSingle();
+            Container.BindFactory<TKey, TContract, PlaceholderFactory<TKey, TContract>>().FromMethod(CreateEnemy);
+        }
+
+
+        private TContract CreateEnemy(DiContainer container, TKey type)
+        {
+            var dictionary = Container.Resolve<Dictionary<TKey, TContract>>();
+            if (!dictionary.TryGetValue(type, out var enemy))
+                Debug.LogError($" key not found {type}");
+            return container.InstantiatePrefabForComponent<TContract>(enemy);
+        }
+
+    }
+
+
+
+
+}
